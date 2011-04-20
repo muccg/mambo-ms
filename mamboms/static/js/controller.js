2f2e181fc65bece504d151a9e89eaf968ea56507
google <google@adamharvey.name>
2011-04-20 01:49:55 +0000

    Fix issues 51, 53 and 54 on Google Code.

--- mamboms/static/js/controller.js
+++ mamboms/static/js/controller.js
@@ -1,98 +1,120 @@
-
-Ext.madasChangeMainContent = function(contentName, paramArray){
-    if (contentName === undefined) {
-        contentName = "home";
-    }
-
-    switch (contentName) {
-       case "login":
-            window.location.href = 'login/';
-            break;
-            
-       case "admin:refdata":
-            window.location.href = 'msadmin/';
-            break;
-             
-       case "admin:usersearch":
-            Ext.madasUserSearchInit();
-            Ext.getCmp('center-panel').layout.setActiveItem('usersearch-panel');
-            break;
-            
-        case "admin:useredit":
-            Ext.madasAdminUserEditInit(paramArray);
-            Ext.getCmp('center-panel').layout.setActiveItem('adminuseredit-container-panel');
-            break;
-            
-        case "user:myaccount":
-            Ext.madasUserEditInit(paramArray);
-            Ext.getCmp('center-panel').layout.setActiveItem('useredit-container-panel');
-            break;
-            
-        case "notauthorized":
-            Ext.getCmp('center-panel').layout.setActiveItem('notauthorized-panel');
-            break;
-            
-        case "message":
-            affectMenu = false;
-            Ext.madasMessage(paramArray);
-            break;
-
-        case "home": 
-        case "search:bykeyword":
-        	Ext.madasSearchByKeywordPanelInit();
-        	Ext.getCmp('center-panel').layout.setActiveItem('searchbykeyword-container-panel');
-        	break;
-  
-        case "search:byspectra":
-        	Ext.getCmp('center-panel').layout.setActiveItem('searchbyspectra-container-panel');
-        	break;
-
-        case "search:bykeywordresults":
-        	Ext.getCmp('center-panel').layout.setActiveItem('searchbykeyword-results-panel');
-        	break;
-  
-        case "search:byspectraresults":
-        	Ext.getCmp('center-panel').layout.setActiveItem('searchbyspectra-results-panel');
-        	break;
-
-        case "metabolite:uploadgc":
-        	Ext.madasMetaboliteUploadInit();
-        	Ext.getCmp('center-panel').layout.setActiveItem('upload-gcmetabolite-container-panel');
-        	break;
-
-        case "metabolite:uploadlc":
-            Ext.madasLCMetaboliteUploadInit();
-            Ext.getCmp('center-panel').layout.setActiveItem('upload-lcmetabolite-container-panel');
-            Ext.madasLCMetaboliteUploadPostActivate();
-        	break;
-
-        case "metabolite:editgc":
-        	Ext.madasMetaboliteEditInit(paramArray);
-        	Ext.getCmp('center-panel').layout.setActiveItem('edit-gcmetabolite-container-panel');
-        	break;
-
-        case "metabolite:editlc":
-        	Ext.madasLCMetaboliteEditInit(paramArray);
-        	Ext.getCmp('center-panel').layout.setActiveItem('edit-lcmetabolite-container-panel');
-        	break;
-
-        case "metabolite:viewgc":
-        	Ext.madasMetaboliteViewInit(paramArray);
-        	Ext.getCmp('center-panel').layout.setActiveItem('view-gcmetabolite-container-panel');
-        	break;
-
-        case "metabolite:viewlc":
-        	Ext.madasLCMetaboliteViewInit(paramArray);
-        	Ext.getCmp('center-panel').layout.setActiveItem('view-lcmetabolite-container-panel');
-        	break;
-
-        case "dataimport":
-            Ext.madasDataImportInit();
-            Ext.getCmp('center-panel').layout.setActiveItem('dataimport-container-panel');
-            break;
-     }
+/* Set up content handling and history while not polluting the global scope any
+ * more than we have to for backward compatibility. */
+(function(Ext) {
+    // The history of pages visited.
+    var history = [];
     
-};
+    /* The function to actually set the centre panel based on the requested
+     * content name. */
+    var setContent = function(contentName, paramArray) {
+        switch (contentName) {
+           case "login":
+                window.location.href = 'login/';
+                break;
+                
+           case "admin:refdata":
+                window.location.href = 'msadmin/';
+                break;
+                 
+           case "admin:usersearch":
+                Ext.madasUserSearchInit();
+                Ext.getCmp('center-panel').layout.setActiveItem('usersearch-panel');
+                break;
+                
+            case "admin:useredit":
+                Ext.madasAdminUserEditInit(paramArray);
+                Ext.getCmp('center-panel').layout.setActiveItem('adminuseredit-container-panel');
+                break;
+                
+            case "user:myaccount":
+                Ext.madasUserEditInit(paramArray);
+                Ext.getCmp('center-panel').layout.setActiveItem('useredit-container-panel');
+                break;
+                
+            case "notauthorized":
+                Ext.getCmp('center-panel').layout.setActiveItem('notauthorized-panel');
+                break;
+                
+            case "message":
+                affectMenu = false;
+                Ext.madasMessage(paramArray);
+                break;
+
+            case "home": 
+            case "search:bykeyword":
+                Ext.madasSearchByKeywordPanelInit();
+                Ext.getCmp('center-panel').layout.setActiveItem('searchbykeyword-container-panel');
+                break;
+      
+            case "search:byspectra":
+                Ext.getCmp('center-panel').layout.setActiveItem('searchbyspectra-container-panel');
+                break;
+
+            case "search:bykeywordresults":
+                Ext.getCmp('center-panel').layout.setActiveItem('searchbykeyword-results-panel');
+                break;
+      
+            case "search:byspectraresults":
+                Ext.getCmp('center-panel').layout.setActiveItem('searchbyspectra-results-panel');
+                break;
+
+            case "metabolite:uploadgc":
+                Ext.madasMetaboliteUploadInit();
+                Ext.getCmp('center-panel').layout.setActiveItem('upload-gcmetabolite-container-panel');
+                break;
+
+            case "metabolite:uploadlc":
+                Ext.madasLCMetaboliteUploadInit();
+                Ext.getCmp('center-panel').layout.setActiveItem('upload-lcmetabolite-container-panel');
+                Ext.madasLCMetaboliteUploadPostActivate();
+                break;
+
+            case "metabolite:editgc":
+                Ext.madasMetaboliteEditInit(paramArray);
+                Ext.getCmp('center-panel').layout.setActiveItem('edit-gcmetabolite-container-panel');
+                break;
+
+            case "metabolite:editlc":
+                Ext.madasLCMetaboliteEditInit(paramArray);
+                Ext.getCmp('center-panel').layout.setActiveItem('edit-lcmetabolite-container-panel');
+                break;
+
+            case "metabolite:viewgc":
+                Ext.madasMetaboliteViewInit(paramArray);
+                Ext.getCmp('center-panel').layout.setActiveItem('view-gcmetabolite-container-panel');
+                break;
+
+            case "metabolite:viewlc":
+                Ext.madasLCMetaboliteViewInit(paramArray);
+                Ext.getCmp('center-panel').layout.setActiveItem('view-lcmetabolite-container-panel');
+                break;
+
+            case "dataimport":
+                Ext.madasDataImportInit();
+                Ext.getCmp('center-panel').layout.setActiveItem('dataimport-container-panel');
+                break;
+         }
+    };
+
+    // Insert the required functions into the Ext namespace.
+    Ext.madasPreviousMainContent = function() {
+        history.pop();
+        var last = history[history.length - 1];
+        setContent(last[0], last[1]);
+    };
+
+    Ext.madasChangeMainContent = function(contentName, paramArray){
+        if (contentName === undefined) {
+            contentName = "home";
+        }
+
+        // We may want to limit the size of the history at some point.
+        history.push([contentName, paramArray]);
+
+        // Actually make the change.
+        setContent(contentName, paramArray);
+    };
+})(Ext);
 
 /**
  * madasInitApplication
