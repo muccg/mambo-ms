e4a5c30249e65973173724af4e8cb751cce7a300
brad power <brad.power@gmail.com>
2011-08-31 04:50:27 +0000

    Head to tail functionality

--- mamboms/static/js/view_graph.js
+++ mamboms/static/js/view_graph.js
@@ -1,6 +1,5 @@
 
 madasShowGraphWindow = function(id, id_type) {
-    console.log('Normal graph window');
     var param_name = 'compound_id';
     if (id_type == 'spectrum_id') {
         param_name = 'spectrum_id';
@@ -18,7 +17,6 @@ madasShowGraphWindow = function(id, id_type) {
 };
 
 mambomsShowHeadToTailWindow = function(compound_id, queryspectra){
-    console.log('Head to tail window');
     var url = 'mamboms/graph/htt_image/' + compound_id + '/' + queryspectra;
     var width = 1000;
     var height = 700;
