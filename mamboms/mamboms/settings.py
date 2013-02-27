056daa577bfd2450622f29a24be4123c10f30a51
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-02-27 11:54:49 +0800

    Added python-setuptools to requires, writeable to scratch

--- mamboms/mamboms/settings.py
+++ mamboms/mamboms/settings.py
@@ -5,7 +5,7 @@ import logging.handlers
 
 CCG_INSTALL_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-CCG_WRITEABLE_DIRECTORY = os.path.join(CCG_INSTALL_ROOT,"writeable")
+CCG_WRITEABLE_DIRECTORY = os.path.join(CCG_INSTALL_ROOT,"scratch")
 
 DATABASES = {
     'default': {
